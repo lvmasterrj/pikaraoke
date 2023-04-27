756fc64b23386a79a90ee6c91279a977d79109dd
lvmasterrj <lvmasterrj@gmail.com>
2023-04-27 16:43:03 -0300

    minor fix

--- karaoke.py
+++ karaoke.py
@@ -88,7 +88,7 @@ class Karaoke:
         #   self._ = _
 
         self.config_obj = configparser.ConfigParser()
-        self.config_obj.read("config.ini")
+        self.config_obj.read("./config.ini")
         user_lng = self.config_obj.get("USERPREFERENCES", "language")
         self.user_audio_delay = self.config_obj.get("USERPREFERENCES", "audio_delay")
 
