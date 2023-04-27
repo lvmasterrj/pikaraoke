e0fc21bd2e8112caf81e5f86168109ae2bd94a71
lvmasterrj <lvmasterrj@gmail.com>
2023-04-27 16:56:49 -0300

    teste

--- karaoke.py
+++ karaoke.py
@@ -89,7 +89,7 @@ class Karaoke:
 
         self.config_obj = configparser.ConfigParser()
         self.config_obj.read("config.ini")
-        user_lng = self.config_obj.get("aUSERPREFERENCES", "language")
+        user_lng = self.config_obj.get("USERPREFERENCES", "language")
         self.user_audio_delay = self.config_obj.get("USERPREFERENCES", "audio_delay")
 
         trans = gettext.translation(
