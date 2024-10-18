efe7cd53bf5608d79efe2a7593f1237423408709
Leonardo Moreira <lvmasterrj@gmail.com>
2024-10-18 18:01:09 -0300

    Bluetooth test

--- testes.py
+++ testes.py
@@ -1,27 +1,50 @@
+# TODO: fazer uma lista de dispositivos já pareados para comparar com a lista de devices, porque se fizer pesquisas rápido, a lista de devices contém dispositivos novos
+
 import subprocess
 import time
 
-def scan_and_get_devices_only():
+def run_bluetoothctl_command(command):
     # Inicia o processo bluetoothctl
-    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    process = subprocess.Popen(['bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, error = process.communicate(command.encode())
+    return output.decode()
 
-    # Envia o comando 'scan bredr' para iniciar a busca por dispositivos Bluetooth
-    process.stdin.write('scan bredr\n')
-    process.stdin.flush()
+def run_commands(commands):
+    for command in commands:
+        output = run_bluetoothctl_command(command)
+        time.sleep(2)
+    return output
 
-    # Aguarda por 5 segundos para realizar a varredura
-    time.sleep(10)
+def scan_and_get_devices(time=10):
 
-    # Envia o comando 'scan off' para parar a varredura
-    process.stdin.write('scan off\n')
-    process.stdin.flush()
+    run_commands(['power on', 'agent on', 'default-agent', 'scan bredr\n'])
 
-    # Envia o comando 'devices' para listar os dispositivos conhecidos
-    process.stdin.write('devices\n')
-    process.stdin.flush()
+    time.sleep(time)
 
-    # Coleta apenas a saída do comando 'devices'
-    devices_output, _ = process.communicate()
+    devices_output = run_commands(['scan off\n', 'devices\n'])
+   
+   
+   
+   
+   
+    # # Envia o comando 'scan bredr' para iniciar a busca por dispositivos Bluetooth
+    # run_bluetoothctl_command(command)
+    # process.stdin.write('scan bredr\n')
+    # process.stdin.flush()
+
+    # # Aguarda pelo tempo requerido ou 10s para realizar a varredura
+    # time.sleep(time)
+
+    # # Envia o comando 'scan off' para parar a varredura
+    # process.stdin.write('scan off\n')
+    # process.stdin.flush()
+
+    # # Envia o comando 'devices' para listar os dispositivos conhecidos
+    # process.stdin.write('devices\n')
+    # process.stdin.flush()
+
+    # # Coleta apenas a saída do comando 'devices'
+    # devices_output, _ = process.communicate()
 
     # Filtra e captura apenas as linhas que contenham 'Device', ou seja, os dispositivos reais
     devices_new = []
@@ -48,70 +71,26 @@ def scan_and_get_devices_only():
                     newline = ['nown', line[1], ' '.join(line[2:])]
                     devices_known.append(newline)
 
-    # temp_devices = []
-    # for device in devices_nown:
-    #     if not any(device[1] in sublist for sublist in devices_new):
-    #         device[0] = "nown_off"
-    #         temp_devices.append(device)
-
-    # Atualizando a lista de dispositivos conhecidos
-    # devices_nown = [device for device in devices_nown if device not in temp_devices]
-
     # Itera sobre a lista de dispositivos conhecidos
-    for device in devices_known[:]:  # Usando [:] para fazer uma cópia da lista durante a iteração
+    for known_device in devices_known[:]:  # Usando [:] para fazer uma cópia da lista durante a iteração
         found = False
         # Verifica se o dispositivo conhecido está na lista de dispositivos novos
         for new_device in devices_new:
-            if device[1] == new_device[1]:  # Compara o endereço MAC
+            if known_device[1] == new_device[1]:  # Compara o endereço MAC
                 # Se o dispositivo estiver na lista de dispositivos novos, remova-o de 'devices_nown'
-                devices_known.remove(device)
+                devices_known.remove(known_device)
                 found = True
                 break
         
         # Se não encontrado, muda 'nown' para 'nown_off'
         if not found:
-            device[0] = 'nown_off'
+            known_device[0] = 'nown_off'
 
-    # print("Nown==============")
-    # print(type(nown_devices))
-    # print(nown_devices)
-    # print("New==============")
-    # print(type(new_devices))
-    # print(new_devices)
-    
-    # for device in nown_devices:
-    #     if 
-
-        #     print(line)
-
-
-
-        # if 'Device' in line:
-        #     line = line.split()
-        #     print(line)
-
-
-
-
-            # devices.append(line)
-            # if line[0] == '[NEW]':
-
-
-
-
-            
-            # if line[0] == 'Device':
-            #     line = line.remove[0]
-            #     print(line)
-            #     if line[0] != line[1]:
-            #         devices.append(line)
-
-    # Retorna a lista de dispositivos
     return devices_new + devices_known
 
-# Executa a função e imprime os dispositivos encontrados
-devices = scan_and_get_devices_only()
+# def connect_to_device(mac_number):
 
-print("Devices")
+devices = scan_and_get_devices()
+
 for device in devices:
     print(device)
