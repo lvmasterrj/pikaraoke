08f37ff3b6212a10693591f9a593c3272c3a069c
Leonardo Moreira <lvmasterrj@gmail.com>
2024-10-18 16:08:12 -0300

    Bluetooth test

--- testes.py
+++ testes.py
@@ -37,16 +37,19 @@ def scan_and_get_devices_only():
             # print(line)
             if line[0] == '\x1b[K[\x01\x1b[0;92m\x02NEW\x01\x1b[0m\x02]':
                 if line[2] != line[3].replace('-', ':'):
-                    line = line.remove[0]
+                    # line = line.remove[0]
                     new_devices.append(line)
 
             if line[0] == 'Device':
                 if line[1] != line[2].replace('-', ':'):
                     nown_devices.append(line)
 
-    print("TESTE==============")
+    print("Nown==============")
     print(type(nown_devices))
     print(nown_devices)
+    print("New==============")
+    print(type(new_devices))
+    print(new_devices)
     
     # for device in nown_devices:
     #     if 
