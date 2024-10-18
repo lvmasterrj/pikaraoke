f9ac68d5c6fe19b4b4081fc40a9d8bd2e68930d8
Leonardo Moreira <lvmasterrj@gmail.com>
2024-10-18 16:06:27 -0300

    Bluetooth test

--- testes.py
+++ testes.py
@@ -26,6 +26,8 @@ def scan_and_get_devices_only():
     # Filtra e captura apenas as linhas que contenham 'Device', ou seja, os dispositivos reais
     new_devices = []
     nown_devices = []
+    nown_off_devices = []
+
 
     lines = devices_output.splitlines()
 
@@ -35,11 +37,19 @@ def scan_and_get_devices_only():
             # print(line)
             if line[0] == '\x1b[K[\x01\x1b[0;92m\x02NEW\x01\x1b[0m\x02]':
                 if line[2] != line[3].replace('-', ':'):
+                    line = line.remove[0]
                     new_devices.append(line)
 
             if line[0] == 'Device':
                 if line[1] != line[2].replace('-', ':'):
                     nown_devices.append(line)
+
+    print("TESTE==============")
+    print(type(nown_devices))
+    print(nown_devices)
+    
+    # for device in nown_devices:
+    #     if 
 
         #     print(line)
 
